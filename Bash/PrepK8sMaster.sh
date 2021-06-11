869e8eede8a885542a6bff0c4f6eb1823baa5bd7
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-06-12 00:01:12 +0700

    Update

--- Bash/PrepK8sMaster.sh
+++ Bash/PrepK8sMaster.sh
@@ -8,9 +8,13 @@ runuser -l ubuntu -c 'mkdir -p $HOME/.kube'
 runuser -l ubuntu -c 'sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config'
 runuser -l ubuntu -c 'sudo chown $(id -u):$(id -g) $HOME/.kube/config'
 
-kubectl apply -f $3
+sleep 1m
 
 kubectl completion bash > /etc/bash_completion.d/kubectl
+
+kubectl apply -f $3
+
+sleep 1m
 
 kubectl get nodes --all-namespaces -o wide --show-labels
 kubectl get pods --all-namespaces -o wide --show-labels
