1ae2966d9fcbe1e93b2efdfcd9ef0191ffa8a356
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-06-12 03:49:04 +0700

    Update

--- Bash/PrepK8sWorker.sh
+++ Bash/PrepK8sWorker.sh
@@ -6,3 +6,9 @@ for arg in "$@"
 do
  runuser -u ubuntu -- ssh -o StrictHostKeyChecking=no $arg sudo `kubeadm token create --print-join-command`
 done
+
+sleep 1m
+
+runuser -l ubuntu -c 'kubectl get nodes --all-namespaces -o wide --show-labels'
+runuser -l ubuntu -c 'kubectl get pods --all-namespaces -o wide --show-labels'
+runuser -l ubuntu -c 'kubectl get services --all-namespaces -o wide --show-labels'
