1fc3f3e2ae654e681f15b7583467d50773f72274
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-06-11 23:31:50 +0700

    Update

--- Bash/PrepK8sMaster.sh
+++ Bash/PrepK8sMaster.sh
@@ -4,6 +4,8 @@ sudo su
 
 kubeadm init --pod-network-cidr=$1 --service-cidr=$2
 
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+runuser -l ubuntu -c 'mkdir -p $HOME/.kube'
+runuser -l ubuntu -c 'sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config'
+runuser -l ubuntu -c 'sudo chown $(id -u):$(id -g) $HOME/.kube/config'
+
+kubectl apply -f $3
