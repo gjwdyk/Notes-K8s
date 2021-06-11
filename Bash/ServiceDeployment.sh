a20681968f7c99fa656579b3433578e7187c3568
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-06-12 02:46:43 +0700

    Update

---
+++ Bash/ServiceDeployment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xe
+
+sudo su
+
+runuser -u ubuntu -- kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.2.0/aio/deploy/recommended.yaml
+
+runuser -u ubuntu -- cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
+
+runuser -u ubuntu -- cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: admin-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
