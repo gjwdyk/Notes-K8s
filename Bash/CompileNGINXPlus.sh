ba6a6d8a6a6b12bd843d5fd2a6a953b21d053ded
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-06-12 23:59:53 +0700

    Update

--- Bash/CompileNGINXPlus.sh
+++ Bash/CompileNGINXPlus.sh
@@ -2,4 +2,17 @@
 
 sudo su
 
+apt install -y make
 
+cd /home/ubuntu
+git clone https://github.com/nginxinc/kubernetes-ingress/
+cd kubernetes-ingress/
+git checkout $1
+
+mv /home/ubuntu/nginx-repo.crt /home/ubuntu/kubernetes-ingress/nginx-repo.crt
+mv /home/ubuntu/nginx-repo.key /home/ubuntu/kubernetes-ingress/nginx-repo.key
+
+docker login -u $2 -p $3
+
+make debian-image-plus PREFIX=$4 TARGET=container
+make push PREFIX=$4
