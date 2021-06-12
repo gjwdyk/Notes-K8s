68df594ea22b1cf66ac636029fc7f4183cf29f9e
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-06-12 19:25:27 +0700

    Update

--- Bash/PrepK8sWorker.sh
+++ Bash/PrepK8sWorker.sh
@@ -9,6 +9,6 @@ done
 
 sleep 1m
 
-runuser -l ubuntu -c 'kubectl get nodes --all-namespaces -o wide --show-labels'
-runuser -l ubuntu -c 'kubectl get pods --all-namespaces -o wide --show-labels'
-runuser -l ubuntu -c 'kubectl get services --all-namespaces -o wide --show-labels'
+runuser -l ubuntu -c 'kubectl get node --all-namespaces -o wide --show-labels'
+runuser -l ubuntu -c 'kubectl get pod --all-namespaces -o wide --show-labels'
+runuser -l ubuntu -c 'kubectl get service --all-namespaces -o wide --show-labels'
