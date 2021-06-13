3248472da521bd4fe88c853e6eea14d3ed32df3e
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-06-13 14:11:03 +0700

    Update

--- Bash/CompileNGINXPlus.sh
+++ Bash/CompileNGINXPlus.sh
@@ -2,17 +2,23 @@
 
 sudo su
 
-apt install -y make
+if [[ $5 != "Yes" ]]; then
+ apt install -y make
+fi
 
 cd /home/ubuntu
 git clone https://github.com/nginxinc/kubernetes-ingress/
 cd kubernetes-ingress/
 git checkout $1
 
-mv /home/ubuntu/nginx-repo.crt /home/ubuntu/kubernetes-ingress/nginx-repo.crt
-mv /home/ubuntu/nginx-repo.key /home/ubuntu/kubernetes-ingress/nginx-repo.key
+if [[ $5 != "Yes" ]]; then
+ mv /home/ubuntu/nginx-repo.crt /home/ubuntu/kubernetes-ingress/nginx-repo.crt
+ mv /home/ubuntu/nginx-repo.key /home/ubuntu/kubernetes-ingress/nginx-repo.key
+fi
 
 docker login -u $2 -p $3
 
-make debian-image-plus PREFIX=$4 TARGET=container
-make push PREFIX=$4
+if [[ $5 != "Yes" ]]; then
+ make debian-image-plus PREFIX=$4 TAG=$1 TARGET=container
+ make push PREFIX=$4 TAG=$1
+fi
--- Bash/PrepNGINXPlusKIC.sh
+++ Bash/PrepNGINXPlusKIC.sh
@@ -2,4 +2,15 @@
 
 sudo su
 
-
+cd ~/kubernetes-ingress/deployments
+runuser -u ubuntu -- kubectl apply -f common/ns-and-sa.yaml
+runuser -u ubuntu -- kubectl apply -f rbac/rbac.yaml
+runuser -u ubuntu -- kubectl apply -f common/default-server-secret.yaml
+runuser -u ubuntu -- kubectl apply -f common/nginx-config.yaml
+runuser -u ubuntu -- kubectl apply -f common/ingress-class.yaml
+runuser -u ubuntu -- kubectl apply -f common/crds/k8s.nginx.org_virtualservers.yaml
+runuser -u ubuntu -- kubectl apply -f common/crds/k8s.nginx.org_virtualserverroutes.yaml
+runuser -u ubuntu -- kubectl apply -f common/crds/k8s.nginx.org_transportservers.yaml
+runuser -u ubuntu -- kubectl apply -f common/crds/k8s.nginx.org_policies.yaml
+runuser -u ubuntu -- kubectl apply -f common/crds/k8s.nginx.org_globalconfigurations.yaml
+runuser -u ubuntu -- kubectl apply -f common/global-configuration.yaml
