52b79b1b4aec018f1c0077995f25005e66be8e93
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-06-15 21:38:48 +0700

    Update

--- Bash/PrepCommonK8s - VersionParameter.sh
+++ Bash/PrepCommonK8s - VersionParameter.sh
@@ -6,4 +6,4 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt update -y
 
-apt install -y kubeadm=1.20.7-00 kubectl=1.20.7-00 kubelet=1.20.7-00 kubernetes-cni=0.8.7-00
+apt install -y kubeadm=$1 kubectl=$2 kubelet=$3 kubernetes-cni=$4
