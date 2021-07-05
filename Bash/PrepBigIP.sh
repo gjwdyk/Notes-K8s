9ba724666894c35bef506520f2fc8aae9e7e836c
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-07-05 16:43:42 +0700

    Update

--- Bash/PrepBigIP.sh
+++ Bash/PrepBigIP.sh
@@ -8,7 +8,7 @@ Loop_Period="2m 2s"
 
 
 
-while ( [ "$Loop" == "Yes" ] ); do
+while ( [ "$Loop" == "Yes" ] ) ; do
  if ssh -o StrictHostKeyChecking=no admin@$BigIPAddress show sys clock ; then echo "`date +%Y%m%d%H%M%S` Success : $?" ; else echo "`date +%Y%m%d%H%M%S` Fail : $?" ; fi
  sleep $Loop_Period
 done
