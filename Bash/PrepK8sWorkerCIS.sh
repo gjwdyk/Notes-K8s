191c5e69501837e4ba4d9a55db598bd4a0df75f1
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-07-16 12:00:18 +0700

    Update

---
+++ Bash/PrepK8sWorkerCIS.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+
+sudo su
+
+for arg in "$@"
+do
+ runuser -u ubuntu -- ssh -o StrictHostKeyChecking=no $arg sudo `kubeadm token create --print-join-command`
+done
+
+sleep 1m
+
+runuser -l ubuntu -c 'kubectl get node --all-namespaces -o wide'
+runuser -l ubuntu -c 'kubectl get pod --all-namespaces -o wide'
+runuser -l ubuntu -c 'kubectl get service --all-namespaces -o wide'
