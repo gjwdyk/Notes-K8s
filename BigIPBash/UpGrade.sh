b84b5552f4b543d4f9c3a6c2aeb7fee508cb46bb
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-07-16 12:42:48 +0700

    Update

--- BigIPBash/UpGrade.sh
+++ BigIPBash/UpGrade.sh
@@ -162,7 +162,7 @@ else
  echo "`date +%Y%m%d%H%M%S` UpGrade Process ."
  if [ -f /shared/images/UpgradeImage.iso ] && [ -f /shared/images/UpgradeImage.iso.md5 ] && [[ `cat $UpGradeImageName_File` =~ ^(BIGIP\-)((([0-9]+)\.)+)([0-9]+)\-((([0-9]+)\.)+)([0-9]+)\.iso$ ]]; then
   if [[ $UpgradeBigIP == "No" ]]; then
-   echo "`date +%Y%m%d%H%M%S` UpGrade was NOT requested ." | tee -a $UpGradeResult
+   echo "`date +%Y%m%d%H%M%S` UpGrade was NOT requested ."
    echo "`date +%Y%m%d%H%M%S` Custom Configuration Finished ." | tee -a $UpGradeResult
   else
    mv /shared/images/UpgradeImage.iso /shared/images/`cat $UpGradeImageName_File`
