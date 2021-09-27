8408cbe81f8e656720a2523706b71b965e472ffe
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-09-27 13:42:47 +0700

    Update

--- Bash/PrepOS.sh
+++ Bash/PrepOS.sh
@@ -2,22 +2,58 @@
 
 sudo su
 
+#
+# Common
+#
 apt install -y apt-transport-https curl
 
 echo "@reboot   root   swapoff -a" >> /etc/crontab
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 swapoff -a
 
+#
+# Docker
+#
 echo "net.ipv4.ip_forward=1" | tee -a /etc/sysctl.conf
 echo "net.bridge.bridge-nf-call-iptables=1" | tee -a /etc/sysctl.conf
 echo "net.bridge.bridge-nf-call-ip6tables=1" | tee -a /etc/sysctl.conf
+
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+#
+# containerd
+#
+printf "overlay\nbr_netfilter" >> /etc/modules-load.d/containerd.conf
+
+echo "net.ipv4.ip_forward=1" | tee -a /etc/sysctl.d/99-kubernetes-cri.conf
+echo "net.bridge.bridge-nf-call-iptables=1" | tee -a /etc/sysctl.d/99-kubernetes-cri.conf
+echo "net.bridge.bridge-nf-call-ip6tables=1" | tee -a /etc/sysctl.d/99-kubernetes-cri.conf
+
+#
+# Common
+#
 modprobe overlay
 modprobe br_netfilter
-echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
 sysctl --system
 sysctl --load
 
+#
+# Test
+#
+echo "/etc/fstab :"
 cat /etc/fstab
+echo "/etc/crontab :"
 cat /etc/crontab
+
+echo "/etc/sysctl.conf :"
 cat /etc/sysctl.conf
+echo "/proc/sys/net/bridge/bridge-nf-call-iptables :"
 cat /proc/sys/net/bridge/bridge-nf-call-iptables
+
+echo "/etc/modules-load.d/containerd.conf :"
+cat /etc/modules-load.d/containerd.conf
+echo "/etc/sysctl.d/99-kubernetes-cri.conf :"
+cat /etc/sysctl.d/99-kubernetes-cri.conf
+
+
