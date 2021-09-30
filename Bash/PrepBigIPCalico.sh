fbfbe2923b06f4b9403af8d34257df84d2d3cee0
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-09-30 11:41:12 +0700

    Update

--- Bash/PrepBigIPCalico.sh
+++ Bash/PrepBigIPCalico.sh
@@ -30,16 +30,16 @@ BigIPAddressPort=$BigIPAddress:$BigIPManagementPort
 
 
 
-if [ -z "$5" ] ; then ; else ; K8sMaster1IPAddress="$5" ; fi
-if [ -z "$6" ] ; then ; else ; K8sWorker1IPAddress="$6" ; fi
-if [ -z "$7" ] ; then ; else ; K8sWorker2IPAddress="$7" ; fi
-if [ -z "$8" ] ; then ; else ; K8sWorker3IPAddress="$8" ; fi
-if [ -z "$9" ] ; then ; else ; K8sWorker4IPAddress="$9" ; fi
-if [ -z "$10" ] ; then ; else ; K8sWorker5IPAddress="$10" ; fi
-if [ -z "$11" ] ; then ; else ; K8sWorker6IPAddress="$11" ; fi
-if [ -z "$12" ] ; then ; else ; K8sWorker7IPAddress="$12" ; fi
-if [ -z "$13" ] ; then ; else ; K8sWorker8IPAddress="$13" ; fi
-if [ -z "$14" ] ; then ; else ; K8sWorker9IPAddress="$14" ; fi
+if [ -z "$5" ] ; then : ; else K8sMaster1IPAddress="$5" ; fi
+if [ -z "$6" ] ; then : ; else K8sWorker1IPAddress="$6" ; fi
+if [ -z "$7" ] ; then : ; else K8sWorker2IPAddress="$7" ; fi
+if [ -z "$8" ] ; then : ; else K8sWorker3IPAddress="$8" ; fi
+if [ -z "$9" ] ; then : ; else K8sWorker4IPAddress="$9" ; fi
+if [ -z "$10" ] ; then : ; else K8sWorker5IPAddress="$10" ; fi
+if [ -z "$11" ] ; then : ; else K8sWorker6IPAddress="$11" ; fi
+if [ -z "$12" ] ; then : ; else K8sWorker7IPAddress="$12" ; fi
+if [ -z "$13" ] ; then : ; else K8sWorker8IPAddress="$13" ; fi
+if [ -z "$14" ] ; then : ; else K8sWorker9IPAddress="$14" ; fi
 
 
 
@@ -61,16 +61,16 @@ if [ "$DEBUG" == "ON" ] ; then
  echo "`date +%Y%m%d%H%M%S` BigIPPodCIDRSubNet=$BigIPPodCIDRSubNet"
  echo "`date +%Y%m%d%H%M%S` BigIPPodCIDR=$BigIPPodCIDR"
  echo "`date +%Y%m%d%H%M%S` VXLANTunnelSelfIP=$VXLANTunnelSelfIP"
- if [ -z "$K8sMaster1IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sMaster1IPAddress=$K8sMaster1IPAddress" ; fi
- if [ -z "$K8sWorker1IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sWorker1IPAddress=$K8sWorker1IPAddress" ; fi
- if [ -z "$K8sWorker2IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sWorker2IPAddress=$K8sWorker2IPAddress" ; fi
- if [ -z "$K8sWorker3IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sWorker3IPAddress=$K8sWorker3IPAddress" ; fi
- if [ -z "$K8sWorker4IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sWorker4IPAddress=$K8sWorker4IPAddress" ; fi
- if [ -z "$K8sWorker5IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sWorker5IPAddress=$K8sWorker5IPAddress" ; fi
- if [ -z "$K8sWorker6IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sWorker6IPAddress=$K8sWorker6IPAddress" ; fi
- if [ -z "$K8sWorker7IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sWorker7IPAddress=$K8sWorker7IPAddress" ; fi
- if [ -z "$K8sWorker8IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sWorker8IPAddress=$K8sWorker8IPAddress" ; fi
- if [ -z "$K8sWorker9IPAddress" ] ; then ; else ; echo "`date +%Y%m%d%H%M%S` K8sWorker9IPAddress=$K8sWorker9IPAddress" ; fi
+ if [ -z "$K8sMaster1IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sMaster1IPAddress=$K8sMaster1IPAddress" ; fi
+ if [ -z "$K8sWorker1IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sWorker1IPAddress=$K8sWorker1IPAddress" ; fi
+ if [ -z "$K8sWorker2IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sWorker2IPAddress=$K8sWorker2IPAddress" ; fi
+ if [ -z "$K8sWorker3IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sWorker3IPAddress=$K8sWorker3IPAddress" ; fi
+ if [ -z "$K8sWorker4IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sWorker4IPAddress=$K8sWorker4IPAddress" ; fi
+ if [ -z "$K8sWorker5IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sWorker5IPAddress=$K8sWorker5IPAddress" ; fi
+ if [ -z "$K8sWorker6IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sWorker6IPAddress=$K8sWorker6IPAddress" ; fi
+ if [ -z "$K8sWorker7IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sWorker7IPAddress=$K8sWorker7IPAddress" ; fi
+ if [ -z "$K8sWorker8IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sWorker8IPAddress=$K8sWorker8IPAddress" ; fi
+ if [ -z "$K8sWorker9IPAddress" ] ; then : ; else echo "`date +%Y%m%d%H%M%S` K8sWorker9IPAddress=$K8sWorker9IPAddress" ; fi
 fi
 
 while ( [ "$Loop" == "Yes" ] ) ; do
@@ -88,18 +88,18 @@ echo "`date +%Y%m%d%H%M%S` Out of Checking Loop for Big-IP."
 
 
 ssh -o StrictHostKeyChecking=no $User@$BigIPAddress modify net self $BigIPAddress/24 allow-service add { tcp:179 }
-
+ssh -o StrictHostKeyChecking=no $User@$BigIPAddress modify net route-domain 0 routing-protocol add { BGP }
 ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \'\!\\nno service password-encryption\\n\!\\nrouter bgp 64512\\n bgp graceful-restart restart-time 120\\n neighbor calico-k8s peer-group\\n neighbor calico-k8s remote-as 64512\\n\' \> /config/zebos/rd0/ZebOS.conf\"
-if [ -z "$K8sMaster1IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sMaster1IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
-if [ -z "$K8sWorker1IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker1IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
-if [ -z "$K8sWorker2IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker2IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
-if [ -z "$K8sWorker3IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker3IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
-if [ -z "$K8sWorker4IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker4IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
-if [ -z "$K8sWorker5IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker5IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
-if [ -z "$K8sWorker6IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker6IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
-if [ -z "$K8sWorker7IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker7IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
-if [ -z "$K8sWorker8IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker8IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
-if [ -z "$K8sWorker9IPAddress" ] ; then ; else ; ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker9IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sMaster1IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sMaster1IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sWorker1IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker1IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sWorker2IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker2IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sWorker3IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker3IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sWorker4IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker4IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sWorker5IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker5IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sWorker6IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker6IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sWorker7IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker7IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sWorker8IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker8IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
+if [ -z "$K8sWorker9IPAddress" ] ; then : ; else ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \' neighbor $K8sWorker9IPAddress peer-group calico-k8s\\n\' \>\> /config/zebos/rd0/ZebOS.conf\" ; fi
 ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \'\!\\nline con 0\\n login\\nline vty 0 39\\n login\\n\!\\nend\\n\' \>\> /config/zebos/rd0/ZebOS.conf\"
 ssh -o StrictHostKeyChecking=no $User@$BigIPAddress restart /sys service tmrouted
 
