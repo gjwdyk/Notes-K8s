a8060af07a6affc73f3d7aa8eab764cca6c87583
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-09-30 13:44:29 +0700

    Update

--- Bash/PrepBigIPCalico.sh
+++ Bash/PrepBigIPCalico.sh
@@ -97,6 +97,10 @@ echo "`date +%Y%m%d%H%M%S` Out of Checking Loop for Big-IP."
 
 
 
+#╔═════════╗
+#║   BGP   ║
+#╚═════════╝
+
 ssh -o StrictHostKeyChecking=no $User@$BigIPAddress modify net self $BigIPAddress/24 allow-service add { tcp:179 }
 ssh -o StrictHostKeyChecking=no $User@$BigIPAddress modify net route-domain 0 routing-protocol add { BGP }
 ssh -o StrictHostKeyChecking=no $User@$BigIPAddress bash -c \"printf \'\!\\nno service password-encryption\\n\!\\nrouter bgp 64512\\n bgp graceful-restart restart-time 120\\n neighbor calico-k8s peer-group\\n neighbor calico-k8s remote-as 64512\\n\' \> /config/zebos/rd0/ZebOS.conf\"
@@ -160,6 +164,89 @@ calicoctl create -f /home/ubuntu/CalicoBGPPeer.yaml
 
 
 
+#╔═══════════╗
+#║   VxLAN   ║
+#╚═══════════╝
+
+ssh -o StrictHostKeyChecking=no $User@$BigIPAddress create auth partition $PartitionName
+ssh -o StrictHostKeyChecking=no $User@$BigIPAddress create net tunnels vxlan $VXLANProfileName { app-service none port 8472 flooding-type none }
+ssh -o StrictHostKeyChecking=no $User@$BigIPAddress create net tunnels tunnel $VXLANTunnelName { app-service none key 1 local-address $BigIPAddress profile $VXLANProfileName }
+ssh -o StrictHostKeyChecking=no $User@$BigIPAddress create net self $VXLANTunnelSelfIPName { address $VXLANTunnelSelfIP vlan $VXLANTunnelName allow-service all }
+
+ssh -o StrictHostKeyChecking=no $User@$BigIPAddress show net tunnels tunnel $VXLANTunnelName all-properties
+BigIPMAC=`ssh -o StrictHostKeyChecking=no $User@$BigIPAddress show net tunnels tunnel $VXLANTunnelName all-properties | egrep -o "([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}"`
+if [ "$DEBUG" == "ON" ] ; then echo "`date +%Y%m%d%H%M%S` BigIPMAC=$BigIPMAC" ; fi
+
+
+
+git clone -b develop https://github.com/gjwdyk/f5-agility-labs-containers.git ~/agilitydocs
+cd ~/agilitydocs/docs/class1/kubernetes
+
+kubectl create secret generic bigip-login -n kube-system --from-literal=username=$User --from-literal=password=$Password
+kubectl create serviceaccount k8s-bigip-ctlr -n kube-system
+kubectl create clusterrolebinding k8s-bigip-ctlr-clusteradmin --clusterrole=cluster-admin --serviceaccount=kube-system:k8s-bigip-ctlr
+
+
+
+sed s+0.0.0.0/0+$BigIPPodCIDR+g /home/ubuntu/BigIPCISNodeTemplate > /home/ubuntu/Temporary1.yaml
+sed s/00:00:00:00:00:00/$BigIPMAC/g /home/ubuntu/Temporary1.yaml > /home/ubuntu/Temporary2.yaml
+sed s/0.0.0.0/$BigIPAddress/g /home/ubuntu/Temporary2.yaml > /home/ubuntu/BigIPCISNode.yaml
+rm /home/ubuntu/Temporary*.yaml
+
+kubectl create -f /home/ubuntu/BigIPCISNode.yaml
+
+
+
+sed s+0.0.0.0:0+$BigIPAddressPort+g /home/ubuntu/BigIPCISClusterDeploymentTemplate > /home/ubuntu/Temporary1.yaml
+sed s/kubernetes-partition/$PartitionName/g /home/ubuntu/Temporary1.yaml > /home/ubuntu/Temporary2.yaml
+sed s/flannel-tunnel/$VXLANTunnelName/g /home/ubuntu/Temporary2.yaml > /home/ubuntu/BigIPCISClusterDeployment.yaml
+rm /home/ubuntu/Temporary*.yaml
+
+kubectl create -f /home/ubuntu/BigIPCISClusterDeployment.yaml
+
+Loop="Yes"
+while ( [ "$Loop" == "Yes" ] ) ; do
+ if [ `kubectl get pods --all-namespaces | grep -i '\<k8s-bigip-ctlr' | grep -i 'Running' | wc -l` -ge 1 ] ; then
+  echo "`date +%Y%m%d%H%M%S` Pod k8s-bigip-ctlr is running."
+  Loop="No"
+ else
+  echo "`date +%Y%m%d%H%M%S` Waiting for pod k8s-bigip-ctlr to run."
+  sleep $Loop_Period
+ fi
+done
+
+
+
+#╔══════════╗
+#║   Test   ║
+#╚══════════╝
+
+sed 's/replicas: 2/replicas: 9/g' /home/ubuntu/agilitydocs/docs/class1/kubernetes/deployment-hello-world.yaml > /home/ubuntu/deployment-hello-world.yaml
+kubectl create -f /home/ubuntu/deployment-hello-world.yaml
+
+Loop="Yes"
+while ( [ "$Loop" == "Yes" ] ) ; do
+ if [ `kubectl get pods --all-namespaces | grep -i '\<f5-hello-world-web' | grep -i 'Running' | wc -l` -ge 9 ] ; then
+  echo "`date +%Y%m%d%H%M%S` ALL pod f5-hello-world-web are running."
+  Loop="No"
+ else
+  echo "`date +%Y%m%d%H%M%S` Waiting for one or more pod f5-hello-world-web to run."
+  sleep $Loop_Period
+ fi
+done
+
+
+
+cp /home/ubuntu/agilitydocs/docs/class1/kubernetes/clusterip-service-hello-world.yaml /home/ubuntu/clusterip-service-hello-world.yaml
+kubectl create -f /home/ubuntu/clusterip-service-hello-world.yaml
+
+sed s/kubernetes/$PartitionName/g /home/ubuntu/agilitydocs/docs/class1/kubernetes/ingress-hello-world.yaml > /home/ubuntu/Temporary1.yaml
+sed s/10.1.1.4/$BigIPAddress/g /home/ubuntu/Temporary1.yaml > /home/ubuntu/ingress-hello-world.yaml
+rm /home/ubuntu/Temporary*.yaml
+kubectl create -f /home/ubuntu/ingress-hello-world.yaml
+
+
+
 #╔═══════════════════╗
 #║   Review Status   ║
 #╚═══════════════════╝
@@ -169,6 +256,9 @@ sleep $Loop_Period
 calicoctl get nodes
 calicoctl get bgpPeer
 ssh -o StrictHostKeyChecking=no $User@$BigIPAddress imish -e \'show bgp neighbor\'
+
+ssh -o StrictHostKeyChecking=no $User@$BigIPAddress show net fdb tunnel $VXLANTunnelName
+ssh -o StrictHostKeyChecking=no $User@$BigIPAddress show net arp all
 
 kubectl get node -o wide -A
 kubectl get deployment -o wide -A
