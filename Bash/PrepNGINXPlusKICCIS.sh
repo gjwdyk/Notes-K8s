3708f5c504e231c1299481980459628738bd0e41
何も無い <64021730+gjwdyk@users.noreply.github.com>
2021-10-04 19:32:38 +0700

    Update

--- Bash/PrepNGINXPlusKICCIS.sh
+++ Bash/PrepNGINXPlusKICCIS.sh
@@ -8,12 +8,7 @@ echo "Executing $0"
 #║   Review Status   ║
 #╚═══════════════════╝
 
-# sleep 1m
 
-kubectl get node -o wide -A
-kubectl get deployment -o wide -A
-kubectl get pod -o wide -A
-kubectl get service -o wide -A
 
 #╔═════════╗
 #║   End   ║
